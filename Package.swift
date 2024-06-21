fcd0ff8000169495a9fb728ffa9d07266a50fb10
Janakiraman-Valuelabs <janakiraman.k@valuelabs.com>
2024-06-21 08:06:22 +0530

    Package changes added.

--- Package.swift
+++ Package.swift
@@ -1,19 +1,16 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "ios-360-videos",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ios-360-videos",
             targets: ["ios-360-videos"]),
     ],
+  dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ios-360-videos"),
         .testTarget(
